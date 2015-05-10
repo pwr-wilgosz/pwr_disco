a53a09aa01c65a63363107131c70228ddf098cf2
Sebastian Wilgosz <sebastian.wilgosz@gmail.com>
2015-05-10 07:26:12 +0200

    Initial Commit

---
+++ pwrdisco/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pwrdisco
+//
+//  Created by Sebastian Wilgosz on 10/05/15.
+//  Copyright (c) 2015 Sebastian Wilgosz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
